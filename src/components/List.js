3f5745bf03fe8b8a7df5e4ef1dceecfc6d33dd40
brian <koreanderson@gmail.com>
2021-11-25 21:42:48 -0500

    Add completion tracking on click

--- src/components/List.js
+++ src/components/List.js
@@ -7,30 +7,43 @@ class List extends Component {
     this.state = { 
       listItems: [
         {
+          index: 0,
           content: 'To do item 1.',
           complete: false,
         },
         {
+          index: 1,
           content: 'To do item 2.',
-          complete: false,
+          complete: true,
         }
       ]
     }
+
+    this.onChildClick = this.onChildClick.bind(this);
+    this.toggleItemComplete = this.toggleItemComplete.bind(this);
+
   }
   render() { 
     return ( 
       <div>
         <h1>ToDo List</h1>
         {this.state.listItems.map((item, i) => {
-          return <ListItem content={item.content} complete={item.complete} key={i}></ListItem>
+          return <ListItem content={item.content} complete={item.complete} key={i} index={this.index} onClick={() => this.onChildClick(i)}></ListItem>
         })}
-
       </div>
     );
   }
 
-  renderListItems() {
-    return 
+  onChildClick(i) {
+    this.toggleItemComplete(i);
+  }
+
+  toggleItemComplete(index) {
+    console.log(index);
+    let listItems = this.state.listItems;
+    let listItem = listItems[index];
+    listItem.complete = !listItem.complete;
+    this.setState({listItems})
   }
 }
  
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,19 +1,11 @@
 import { Component } from "react";
 
 class ListItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      listItems: [{
-        content: 'To do item.',
-        complete: false,
-      }]
-    }
-  }
+
   render() { 
     return ( 
       <div>
-        <div>{this.props.content}</div>
+        <div onClick={this.props.onClick}>{this.props.content} {this.props.complete ? 'Complete' : 'Incomplete'}</div>
       </div>
     );
   }
